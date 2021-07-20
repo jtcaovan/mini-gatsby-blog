4b83ac944b8536f24b0a1f7201ed8fc3e2f1783d
Justin Caovan <jtcaovan@gmail.com>
2021-07-20 16:49:05 -0700

    added CSS styling to layout

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+function AboutPage() {
+    return (
+        <Layout pageTitle='About Me'>
+            <p>Hi, my name is Justin</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
