7082dbc239ca6513ef645db46e725072510c17d2
Justin Caovan <jtcaovan@gmail.com>
2021-07-21 11:06:00 -0700

    display name of blog posts

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,6 +23,8 @@ const Layout = ({ pageTitle, children }) => {
                         <Link to='/' className={navLinkText}>Home</Link></li>
                     <li className={navLinkItem}>
                         <Link to='/about' className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}>
+                        <Link to='/blog' className={navLinkText}>Blog</Link></li>
                 </ul>
             </nav>
             <h1 className={heading}>{pageTitle}</h1>
