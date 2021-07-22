ebb0f0770caf1bc9ba5211c0dc3026fb44e8e751
Justin Caovan <jtcaovan@gmail.com>
2021-07-21 18:27:23 -0700

    added blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Blog",
+    title: "The Cheat Sheet",
   },
   plugins: ["gatsby-plugin-gatsby-cloud",
   "gatsby-plugin-image",
   "gatsby-plugin-sharp",
+  "gatsby-plugin-mdx",
   {
     resolve: "gatsby-source-filesystem",
     options: {
